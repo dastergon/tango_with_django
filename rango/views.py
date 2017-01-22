ff18dd25c8be04613ff64ed3ea08c165014a8135
Pavlos Ratis <2286121R@student.gla.ac.uk>
2017-01-22 23:34:46 +0000

    add support for most views pages(ch6)

--- rango/views.py
+++ rango/views.py
@@ -4,8 +4,11 @@ from django.http import HttpResponse
 from rango.models import Category, Page
 
 def index(request):
+    context_dict = {}
     category_list = Category.objects.order_by('-likes')[:5]
-    context_dict = {'categories': category_list}
+    page_list = Page.objects.order_by('-views')[:5]
+    context_dict['categories'] = category_list
+    context_dict['pages'] = page_list
     return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
