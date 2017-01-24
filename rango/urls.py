0a807ef42c51c14aa69f27070363a9939409bb48
Pavlos Ratis <2286121R@student.gla.ac.uk>
2017-01-24 00:25:31 +0000

    add support for login

--- rango/urls.py
+++ rango/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 from rango import views
 
 urlpatterns = [
+    url(r'^login/$', views.user_login, name='login'),
     url(r'^register/$', views.register, name='register'),
     url(r'^category/(?P<category_name_slug>[\w\-]+)/add_page/', views.add_page, name='add_page'),
     url(r'^add_category/$', views.add_category, name='add_category'),
--- rango/views.py
+++ rango/views.py
@@ -88,3 +88,20 @@ def register(request):
                 {'user_form': user_form,
                 'profile_form': profile_form,
                 'registered': registered})
+
+def user_login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(username=username, password=password)
+        if user:
+            if user.is_active:
+                login(request, user)
+                return HttpResponseRedirect(reverse('index'))
+            else:
+                return HttpResponse("Your Rango account is disabled.")
+        else:
+            print("Invalid login details: {0}, {1}".format(username, password))
+            return HttpResponse("Invalid login details supplied.")
+    else:
+        return render(request, 'rango/login.html', {})
