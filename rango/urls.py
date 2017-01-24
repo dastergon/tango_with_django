a6fadb5b00966b6990a1d32f88ba87f2102fedcc
Pavlos Ratis <2286121R@student.gla.ac.uk>
2017-01-24 00:15:30 +0000

    add support for registration

--- rango/urls.py
+++ rango/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 from rango import views
 
 urlpatterns = [
+    url(r'^register/$', views.register, name='register'),
     url(r'^category/(?P<category_name_slug>[\w\-]+)/add_page/', views.add_page, name='add_page'),
     url(r'^add_category/$', views.add_category, name='add_category'),
     url(r'^category/(?P<category_name_slug>[\w\-]+)/$',
